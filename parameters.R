34b8727ae60c06c182c087afd568e4ab3da0b664
Peng <gangpeng@in-ota-r3y7cq215v.attlocal.net>
2023-06-23 10:19:59 -0400

    update parameters.R

--- parameters.R
+++ parameters.R
@@ -1,4 +1,4 @@
-load("~/Downloads/sampleInfo.RData")
+#load("~/Downloads/sampleInfo.RData")
 
 sample_info <- data.frame(sample_info)
 
@@ -11,3 +11,20 @@ categoricalVariables <- c("Age_group", "BW_group", "GA_group", "sex", "TPN", "ra
 
 
 
+variable_info <- data.frame(
+  variables = c(
+    "Age_hr", "Age_group", "BW", "BW_group",
+    "GA", "GA_group", "sex", "TPN",
+    "race_major", "race_detail", "RBC_transfusion", "Mom_age"),
+  varShow = c(
+    "Age at blood collection (Hour)", "Age at blood collection (Group)", "Birth Weight (g)", "Birth Weight (Group)",
+    "Gestational age (weeks)", "Gestational age (Group)", "Sex", "Total Parenteral Nutrition",
+    "Ethnicity (Major)", "Ethnicity (Detailed)", "Blood transfusion", "Maternal age"
+  ),
+  varType = c(
+    "continuous", "categorical",  "continuous", "categorical",
+    "continuous", "categorical", "categorical", "categorical",
+    "categorical", "categorical", "categorical", "continuous"
+  ),
+  stringsAsFactors = FALSE
+)
